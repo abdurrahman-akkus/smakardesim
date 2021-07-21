9d28a19ce2756d71c18ac8c02ffef96236fb84a0
Ahmet Akkuş <ahmedakkus.42@gmail.com>
2021-07-22 01:14:12 +0300

    karışık işler

--- cocuklarimiz.php
+++ cocuklarimiz.php
@@ -1,1 +1,161 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
 
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Çocuklarımız</title>
+    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="css/cocuklarimiz.css" media="screen">
+    <link rel="stylesheet" href="css/custom.css" media="screen">
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.15.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/anasayfa.css" media="screen">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": "SMA Kardeşim",
+        "url": "index.php",
+        "logo": "images/default-logo.png"
+    }
+    </script>
+    <meta property="og:title" content="Çocuklarımız">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.php">
+    <meta property="og:url" content="index.php">
+</head>
+
+<body class="u-body">
+    <main>
+        <header class="u-clearfix u-header u-header" id="sec-2c14">
+            <div class="u-clearfix u-sheet u-sheet-1">
+                <a href="/" class="u-image u-logo u-image-1">
+                    <img src="images/smaKardesimLogo.svg" class="u-logo-image u-logo-image-1">
+                </a>
+                <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+                    <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; text-transform: uppercase; font-weight: 500;">
+                        <a class="u-button-style u-custom-active-border-color u-custom-active-color u-custom-border u-custom-border-color u-custom-borders u-custom-hover-border-color u-custom-hover-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+                            <svg>
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use>
+                            </svg>
+                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <defs>
+                                    <symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;">
+                                        <rect y="1" width="16" height="2"></rect>
+                                        <rect y="7" width="16" height="2"></rect>
+                                        <rect y="13" width="16" height="2"></rect>
+                                    </symbol>
+                                </defs>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="u-custom-menu u-nav-container">
+                        <ul class="u-nav u-spacing-2 u-unstyled u-nav-1">
+                            <li class="u-nav-item"><a class="u-active-palette-1-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-button-style u-hover-palette-1-light-1 u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="index.php" style="padding: 10px 20px;">Anasayfa</a>
+                            </li>
+                            <li class="u-nav-item"><a class="u-active-palette-1-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-button-style u-hover-palette-1-light-1 u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="biz-kimiz.php" style="padding: 10px 20px;">BİZ KİMİZ</a>
+                            </li>
+                            <li class="u-nav-item"><a class="u-active-palette-1-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-button-style u-hover-palette-1-light-1 u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="iletisim.php" style="padding: 10px 20px;">İLETİŞİM</a>
+                            </li>
+                            <li class="u-nav-item"><a class="u-active-palette-1-base u-border-active-palette-1-base u-border-hover-palette-1-light-1 u-button-style u-hover-palette-1-light-1 u-nav-link u-text-active-white u-text-grey-90 u-text-hover-white" href="yonetim/index.php" style="padding: 10px 20px;">GİRİŞ YAP</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="u-custom-menu u-nav-container-collapse">
+                        <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                            <div class="u-sidenav-overflow">
+                                <div class="u-menu-close"></div>
+                                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.php" style="padding: 10px 20px;">Anasayfa</a>
+                                    </li>
+                                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="biz-kimiz.php" style="padding: 10px 20px;">BİZ KİMİZ</a>
+                                    </li>
+                                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="iletisim.php" style="padding: 10px 20px;">İLETİŞİM</a>
+                                    </li>
+                                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="yonetim/index.php" style="padding: 10px 20px;">GİRİŞ YAP</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <section class="u-clearfix u-section-3" id="sec-468f">
+            <h1 class="u-text u-title">TÜM ÇOCUKLARIMIZ</h1>
+            <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+                <div class="u-list u-list-1">
+                    <div class="u-repeater u-repeater-1">
+                        <div class="cocuk">
+                            <div class="badge-container">
+                                <span class="badge rounded-pill bg-info">SMA-1</span>
+                                <span class="badge rounded-pill bg-secondary"><i class="fas fa-sync-alt"></i> 4g</span>
+                            </div>
+                            <div class="u-container-style back-img-container u-list-item u-repeater-item u-shading" data-image-width="2000" data-image-height="1333">
+                                <img src="images/3.jpeg" class="back-img">
+                                <div class="u-container-layout u-similar-container u-valign-top u-container-layout-1">
+                                    <h3 class="u-text u-text-1">Sample Headline</h3>
+                                    <p class="u-text u-text-2">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                </div>
+                            </div>
+                            <div style="margin-top: -5px">
+                                <div class="a-progress orange">
+                                    <div class="a-progress-bar" val="45" style="width:45%; background:#f7810e;">
+                                        <div class="a-progress-value"><span>45</span>%</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cocuk">
+                            <div class="u-container-style back-img-container u-list-item u-repeater-item u-shading" data-image-width="2000" data-image-height="1333">
+                                <img src="images/3.jpeg" class="back-img">
+                                <div class="u-container-layout u-similar-container u-valign-top u-container-layout-2">
+                                    <h3 class="u-text u-text-3">Sample Headline</h3>
+                                    <p class="u-text u-text-4">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                </div>
+                            </div>
+                            <div style="margin-top: -5px">
+                                <div class="a-progress orange">
+                                    <div class="a-progress-bar" val="70" style="width:70%; background:#f7810e;">
+                                        <div class="a-progress-value"><span>70</span>%</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="cocuk">
+                            <div class="u-container-style back-img-container u-list-item u-repeater-item u-shading" data-image-width="2000" data-image-height="1333">
+                                <img src="images/3.jpeg" class="back-img">
+                                <div class="u-container-layout u-similar-container u-valign-top u-container-layout-3">
+                                    <h3 class="u-text u-text-1">Sample Headline</h3>
+                                    <p class="u-text u-text-6">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                                </div>
+                            </div>
+                            <div style="margin-top: -5px">
+                                <div class="a-progress orange" onshow="animate();">
+                                    <div class="a-progress-bar" val="90" style="width:90%; background:#f7810e;">
+                                        <div class="a-progress-value"><span>90</span>%</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="u-align-left u-clearfix u-footer" id="sec-0463">
+            <div class="u-clearfix u-sheet u-sheet-1"></div>
+        </footer>
+    </main>
+</body>
+
+</html>
